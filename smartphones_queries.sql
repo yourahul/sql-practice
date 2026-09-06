efcb8a8b94e7a43baa19a442766f6ed3ea7b9865
Rahul U <rahulpoojari120@gmail.com>
2026-09-06 16:18:58 +0530

    Update smartphones_queries.sql

--- smartphones_queries.sql
+++ smartphones_queries.sql
@@ -27,3 +27,32 @@ group by brand_name
 having avg_rating > 75
 order by avg_rating desc
 Limit 5
+
+--6/9/26--
+
+Q6
+SELECT model,brand_name,price, rating FROM campusx.smartphones_cleaned_v6
+where rating > 80 and price < 25000
+order by rating desc
+
+Q7
+SELECT max(price),min(price),avg(price) FROM campusx.smartphones_cleaned_v6
+where has_5g = 'True'
+
+Q8
+SELECT brand_name, count(*) FROM campusx.smartphones_cleaned_v6
+group by brand_name
+order by count(*) desc
+
+Q9
+SELECT BattingTeam, sum(total_run) FROM campusx.ipl
+group by BattingTeam
+having  sum(total_run) > 10000
+
+Q10
+SELECT brand_name, avg(price), count(*) FROM campusx.smartphones_cleaned_v6
+group by brand_name
+Having count(*) >= 5
+order by avg(price) desc
+Limit 5
+
