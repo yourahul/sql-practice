de509d7adb7bc53baf3ab6906fae8c5be74db288
Rahul U <rahulpoojari120@gmail.com>
2026-09-06 16:21:24 +0530

    Update joins_queries.sql

--- joins_queries.sql
+++ joins_queries.sql
@@ -45,3 +45,47 @@ join flipkart.category t2
 on t1.category_id = t2.category_id
 group by t2.vertical
 having sum(profit) > 4000
+
+Q6
+SELECT t1.brand_name,
+	   t1.model,
+       t2.model,
+       t1.rating,
+       t2.rating 
+FROM campusx.smartphones_cleaned_v6 t1
+join campusx.smartphones_cleaned_v6 t2
+on t1.brand_name = t2.brand_name
+AND t1.model < t2.model
+WHERE t1.rating > 80 
+AND t2.rating > 80;
+
+Q7
+SELECT t1.brand_name,t1.processor_brand,t1.model,t2.model FROM campusx.smartphones_cleaned_v6 t1
+JOIN campusx.smartphones_cleaned_v6 t2
+ON t1.brand_name = t2.brand_name
+AND t1.processor_brand = t2.processor_brand
+WHERE t1.model < t2.model
+
+Q8
+SELECT brand_name 
+FROM campusx.smartphones_cleaned_v6
+where rating >= 85 
+
+UNION
+
+SELECT brand_name 
+FROM campusx.smartphones_cleaned_v6
+where price < 15000;
+
+Q9
+SELECT BattingTeam,sum(total_run) AS TotalRuns 
+FROM campusx.ipl
+GROUP BY BattingTeam
+HAVING SUM(total_run) > 10000
+
+UNION ALL
+
+SELECT BattingTeam,sum(total_run) AS TotalRuns
+FROM campusx.ipl
+GROUP BY BattingTeam
+HAVING SUM(total_run) < 1000;
